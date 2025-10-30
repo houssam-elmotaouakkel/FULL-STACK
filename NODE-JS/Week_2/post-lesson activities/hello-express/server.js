6a7f815d41372db9c9e76b4c6651f9d56b75128a
houssam-elmotaouakkel <houssammoutaouakkel1994@gmail.com>
2025-10-30 14:40:50 +0100

    continue

--- NODE-JS/Week_2/post-lesson activities/hello-express/server.js
+++ NODE-JS/Week_2/post-lesson activities/hello-express/server.js
@@ -8,3 +8,11 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Le serveur ecoute sur le port 3000');
 });
+
+app.get('/api/products', (req, res) => {
+    res.json([{id: 1, name: 'Laptop'}, {id: 2, name: 'phone'}])
+});
+
+app.get('/api/products/:id', (req, res) => {
+    res.json({ message: `produit ${req.params.id}`});
+});
